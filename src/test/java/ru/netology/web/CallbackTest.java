3cffe1313ea2d46c253f270130315bc620b5f2f3
kris-i-kris <flare120491@gmail.com>
2024-01-28 22:07:43 +0700

    ok

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -7,12 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
@@ -44,7 +40,7 @@ public class CallbackTest {
     @Test
     void test() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Анна Иванова-Петрова");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79000000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
